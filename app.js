3dae51a09af4d44a0f00f148aa38b69765faeb63
tatiana.maldonado <tatiana.maldonado@iesonline.com.co>
2025-01-17 16:34:05 -0500

    feat: :sparkles: Se desarrolla el desafio final del curso

--- app.js
+++ app.js
@@ -6,12 +6,32 @@ let intentos = prompt("ingresa el numero de intentos maximos");
 //let palabraVeces = 'vez';
 let maximosIntentos = 6;
 
+console.log(numeroUsuario);
 
 while (numeroUsuario != numeroSecreto) {
     numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`));
 
-console.log(numeroUsuario);
+    console.log(typeof(numeroUsuario));
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos++;
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+    }
 }
