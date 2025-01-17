66ed05798e2231248fc103739f7285bc553b0ba4
tatiana.maldonado <tatiana.maldonado@iesonline.com.co>
2025-01-17 16:33:22 -0500

    feat: :sparkles: Se desarrolla el desafio final del curso

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+//Variables
+let numeroMaximoPosible = prompt(" ingresa el numero maximo posible");
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroUsuario = 0;
+let intentos = prompt("ingresa el numero de intentos maximos");
+//let palabraVeces = 'vez';
+let maximosIntentos = 6;
+
+
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`));
 
 console.log(numeroUsuario);
 
